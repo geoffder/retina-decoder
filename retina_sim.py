9b445729eb5ff87bb864cc14dc4b60c933ef3a03
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-03-13 17:49:18 -0700

    add network margins (cells not on edge of field)

--- retina_sim.py
+++ retina_sim.py
@@ -1,21 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numba
 
 from sim_util import rotate, StackPlotter
 
-'''
+"""
 Need to decide the level of complexity of cell state simulation. Actually model
 a Vm, and have Ca++ channels that respond to it? Or just model the 'activation'
 or 'signal' of the cell that goes up and down with +ve/-ve stimuli?
 -> See learning/neuro folder for simple biophysical cell simulations.
-'''
+"""
 
 
 class NetworkModel(object):
-    def __init__(self, tstop=500, dt=1, dims=[400, 400]):
+    def __init__(self, tstop=500, dt=1, dims=[600, 600]):
         self.tstop = tstop
         self.dt = dt
         self.dims = dims
+        self.margin = 100  # no cells allowed within this distance of edge
         self.origin = (dims[0]//2, dims[1]//2)
         self.t = 0
         self.stims = []
@@ -24,7 +26,8 @@ class NetworkModel(object):
     def populate(self, pop=1):
         self.cells = []
         for i in range(pop):
-            pos = [np.random.randint(0, dim+1) for dim in self.dims]
+            pos = [np.random.randint(self.margin, dim-self.margin+1)
+                   for dim in self.dims]
             self.cells.append(Cell(self, pos=pos))
 
     def newStim(self, type='bar', startPos=[0, 0], tOn=0, tOff=None,
@@ -40,24 +43,24 @@ class NetworkModel(object):
         self.stims.append(stim)
 
     def storeStimMov(self):
-        '''
+        """
         Add together all stims in to one movie and store in a list. Useful
         for experiments with mutliple stimulus presentations.
-        '''
+        """
         movie = np.zeros((*self.dims, self.tstop//self.dt))
         for stim in self.stims:
             movie += np.dstack(stim.rec)
-        self.stimMovies.append(movie)
+        self.stimMovies.append(movie.astype(np.uint8))
 
     def clearStims(self):
         self.stims = []
 
     def step(self):
-        '''
+        """
         Run through a model time-step. First updating positions of stimuli,
         then checking for interactions with each of the cells. Cells also go
         through updates, such as Vm/activation decay.
-        '''
+        """
         for stim in self.stims:
             stim.move()
             for cell in self.cells:
@@ -67,7 +70,8 @@ class NetworkModel(object):
         self.t += self.dt
 
     def run(self):
-        'Run through from t=0 to t=tstop and store data in lists.'
+        "Run through from t=0 to t=tstop and store data in lists."
+        self.t = 0
         # step through experiment until tstop
         for t in range(self.tstop//self.dt):
             self.step()
@@ -77,7 +81,7 @@ class NetworkModel(object):
         self.storeStimMov()
 
     def plotCells(self):
-        'Plot map of cells and their receptive fields.'
+        "Plot map of cells and their receptive fields."
         net = np.zeros(self.dims)
         fig, ax = plt.subplots(1)
         for cell in self.cells:
@@ -90,7 +94,7 @@ class NetworkModel(object):
         return fig, ax
 
     def plotStims(self):
-        'Plot stimulus movies.'
+        "Plot stimulus movies."
         movie = np.dstack(self.stimMovies)
         fig, ax = plt.subplots(1)
         stack = StackPlotter(ax, movie, delta=50)
@@ -98,6 +102,7 @@ class NetworkModel(object):
         return fig, ax, stack
 
     def plotRecs(self):
+        "Plot recording of each cell for whole 'experiment'"
         fig, axes = plt.subplots(len(self.cells), figsize=(6, 8))
         for i, cell in enumerate(self.cells):
             rec = np.concatenate(cell.recs, axis=0)
@@ -107,13 +112,17 @@ class NetworkModel(object):
         return fig, axes
 
     def plotExperiment(self):
+        "Plot cell map, stimulus movie and cell recordings."
         cellFig, cellAx = self.plotCells()
-        stimFig, stimAm, stimStack = self.plotStims()
-        self.plotRecs()
+        stimFig, stimAx, stimStack = self.plotStims()
+        recFig, recAx = self.plotRecs()
         plt.show()
 
 
 class Stim(object):
+    """
+    Create stimulus objects (e.g. circles and bars).
+    """
     def __init__(self, model, type='bar', startPos=[0, 0], tOn=0, tOff=None,
                  vel=0, theta=0, orient=0, amp=1, change=0, width=10,
                  length=100, radius=100):
@@ -165,6 +174,11 @@ class Stim(object):
 
 
 class Cell(object):
+    """
+    Base class for cells, plan to make child Classes for different cell types
+    that have different excite/inhib functions to allow different stimulus
+    preferrences.
+    """
     def __init__(self, model, pos=[0, 0], diam=20, rf=50, dt=1):
         self.model = model  # the network model this cell belongs to
         self.pos = pos
@@ -187,7 +201,7 @@ class Cell(object):
         return mask
 
     def decay(self):
-        'Decay of activation that occurs with each timestep.'
+        "Decay of activation that occurs with each timestep."
         delta = self.Vm * (1 - np.exp(-self.model.dt/self.dtau))
         self.Vm = np.max([0, self.Vm - delta])
         self.rec.append(self.Vm)  # last thing each time step, so record Vm
