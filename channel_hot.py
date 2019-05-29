2ec1e3362578b9923ad9aae1608667b18e743fe0
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-05-28 18:01:38 -0700

    convert to assume time is first dimension. Added tests

--- channel_hot.py
+++ channel_hot.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 def channel_hot_encode(masks, video):
-    video = video.transpose(2, 0, 1)  # move time to first dimension
+    "Take shape (TxHxW) and encode to (TxCxHxW) with list of cluster masks."
     return np.stack([video*mask for mask in masks], axis=1)
 
 
 if __name__ == '__main__':
     masks = [np.random.randn(350, 350)*i for i in range(5)]
-    video = np.random.randn(350, 350, 60)
+    video = np.random.randn(60, 350, 350)
     test = channel_hot_encode(masks, video)
+    print('input shape:', video.shape)
+    print('number of masks:', len(masks))
+    print('output shape:', test.shape)
