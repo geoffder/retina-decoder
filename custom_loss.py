afb26cdc58bea4ac87f36c9b0b5d717b2e47bdfa
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-06-04 16:46:33 -0700

    DecoderLoss to it's own script

---
+++ custom_loss.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+
+class DecoderLoss(nn.Module):
+    """
+    Experimental. Idea is to harshly penalize safe (mean) predictions and
+    encourage the model to try to move it's output towards the non-zero
+    stimulus values, rather than staying near zero always to minimize loss
+    (since most of the video is zero).
+    """
+    def __init__(self, alpha=1):
+        super(DecoderLoss, self).__init__()
+        self.alpha = alpha
+
+    def forward(self, decoding, targets):
+        error = (decoding - targets).pow(2) * (1 + self.alpha*targets.abs())
+        return error.mean()
